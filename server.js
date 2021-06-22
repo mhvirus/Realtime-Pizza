a857ccb67e538cb8f5d5f325d8a16ac4b487cd91
Haris Qasim <harisqasim78@gmail.com>
2021-06-23 02:09:04 +0500

    Created and render homepage

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('hhhhhh');
+    res.render('home');
 })
+
+// set template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, '/resources/views'))
+app.set("view engine", "ejs")
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`)
